48554f0361bfd2b4cd3ba1970afffd2e3b995a7f
atanuc073 <atanuc073@gmail.com>
2024-03-28 12:44:54 +0530

    mongoDb connection

---
+++ test.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+# mongodb+srv://atanu:<password>@ssensor.rqglebz.mongodb.net/?retryWrites=true&w=majority&appName=ssensor
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+uri = "mongodb+srv://atanu:atanu_mongoDB@ssensor.rqglebz.mongodb.net/?retryWrites=true&w=majority&appName=ssensor"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db =client.get_database("ssensor")
+print(db.list_collection_names)
